eafdc0a40ca7dbb18df30cfb5ecee406ed37f257
郑嘉伟 <ashudeyouxi@sina.cn>
2019-03-25 21:29:05 +0800

    gotchas

--- src/index.js
+++ src/index.js
@@ -15,11 +15,13 @@ function component() {
     return element;
 }
 
-document.body.appendChild(component());
-
+let element = component();
+document.body.appendChild(element);
 if (module.hot) {
     module.hot.accept('./print.js', function () {
         console.log('Accepting the updated printMe module!');
-        printMe();
+        document.body.removeChild(element)
+        element = component()
+        document.body.appendChild(element)	
     })
 }
